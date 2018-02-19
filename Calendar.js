dda9de2f79a96769ea4638e3f779ced8e7adcfaa
vladimiregardt <vladimiregardt@gmail.com>
2018-02-19 22:21:59 +0300

    19.02.2018

--- Calendar.js
+++ Calendar.js
@@ -1,9 +1,21 @@
 "use strict";
 
-function createHeader() {
-    var header = document.createElement("h2");
-    header.innerHTML = "Заголовок";
+let createHeaderDom = () => {
+    let header = document.createElement(`div`);
+    header.id = `header`;
+    header.innerHTML = `
+        <div id="navigation-form">
+            <form id="add-refresh-form">
+                <input id="add-refresh-form_add-button" type="submit" value="Добавить">
+                <input id="add-refresh-form_refresh-button" type="submit" value="Обновить"> 
+            </form>
+            <form id="search-form">
+                <input id="search-form_input" type="text" name="search" placeholder="Событие, дата или участник">
+                <input id="search-form_button" type="submit" value="&#x1F50D;"> 
+            </form>
+        </div>
+    `;
     document.body.appendChild(header);
-}
+};
 
-createHeader();
+createHeaderDom();
