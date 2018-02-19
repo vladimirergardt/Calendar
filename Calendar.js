7891558c262498e4c6f3a4869dca0c9b091acb3a
vladimiregardt <vladimiregardt@gmail.com>
2018-02-19 21:44:54 +0300

    19.02.2018

--- Calendar.js
+++ Calendar.js
@@ -1,8 +1,9 @@
-/**
- * Created by User Notebook on 19.02.2018.
- */
 "use strict";
 
-let createHeader = () => {
+function createHeader() {
+    var header = document.createElement("h2");
+    header.innerHTML = "Заголовок";
+    document.body.appendChild(header);
+}
 
-};
+createHeader();
